d3867d9668e5931c9323e9800a5aeda1cdc6253c
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-13 15:02:15 +0000

    [update]パターンとマッチング

--- regular_ex.rb
+++ regular_ex.rb
@@ -1,9 +1,12 @@
+# 正規表現
+# パターンとマッチング
+
 /Ruby/ =~ "Yet Another Ruby Hacker."
 
-names = ["a","b","c"]
+names = ["林","小林","森田","岡本","岡田"]
 
-names.each do |n|
-  if /b/ =~ n
-    p n
+names.each do |name|
+  if /岡/ =~ name
+    p name
   end
 end
