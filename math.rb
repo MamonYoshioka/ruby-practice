e787d2892f4ec321d977bd1096a2d742e080698a
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-13 14:05:55 +0000

    更新

---
+++ math.rb
@@ -0,0 +1,19 @@
+r = Math.sin(3.1415)
+l = Math.sqrt(9)
+
+p r
+p l
+
+
+# 変数にオブジェクトを代入する
+x = 10
+y = 20
+z = 30
+
+# 表面積と体積を計算する
+area = (x*y + y*z + z*x)*2
+volume = x * y * z
+# 計算結果を表示する
+p "表面積=#{area}"
+p "体積=#{volume}"
+
