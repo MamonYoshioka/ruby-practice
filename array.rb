1af6571346840053aa1cc405240d7f8e9a8de78a
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-13 14:33:16 +0000

    配列イテレーター確認

--- array.rb
+++ array.rb
@@ -1,2 +1,19 @@
 names = ["Git", "HTML", "CSS"]
 p names[1]
+
+
+jnames = ["小林","林","高野","森岡"]
+p jnames[0]
+
+p "私の名前は#{jnames[3]}です。"
+
+jnames[4] = "野尻"
+
+p jnames
+p "jnames配列のサイズは #{jnames.size}つ です。"
+
+p "###############"
+jnames.each do |jn|
+  p "名前は#{jn}。"
+end
+p "###############"
