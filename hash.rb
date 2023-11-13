b3464b571535826f18e05c37c8371c6ab25317ea
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-13 14:49:49 +0000

    hash操作確認&更新

--- hash.rb
+++ hash.rb
@@ -1,2 +1,18 @@
 tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
 p tall["太郎"]
+
+person = {name: "takahashi", age: 22}
+p person
+p person[:name]
+p person[:age]
+
+person[:tel] = "000-1234-5678"
+
+p person
+p person[:tel]
+p "####################"
+# イテレーターで繰り返し
+person.each do |k, v|
+  p "#{k}:#{v}"
+end
+p "####################"
