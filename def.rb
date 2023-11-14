d0939eae8467b360e36cce4b26588435c4dde4e2
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-14 12:58:50 +0000

    ローカル変数・グローバル変数スコープの範囲の確認

---
+++ def.rb
@@ -0,0 +1,8 @@
+def hello
+  10.times do
+    p "Hello"
+  end
+end
+
+# hello()
+# 外部(use_hello.rb)からこのファイルの処理を読み込めるように設定する
---
+++ scopetest.rb
@@ -0,0 +1,7 @@
+$x = 0
+x = 0
+
+require_relative "subscopetest"
+
+p $x
+p x
