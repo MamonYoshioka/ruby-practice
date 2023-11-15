ebe317c934bc0acd55b20c0533c0f425067f27d2
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-15 15:05:26 +0000

    break,next文

--- each.rb
+++ each.rb
@@ -1,3 +1,39 @@
 (0..10).each do |num|
   p num
 end
+
+p "@@@@@@@@@@@@@@@@@@@@@@"
+
+# eachメソッド動作復習
+=begin
+eachメソッドはオブジェクトの集まりに対して、それを1つずつ取り出す時に使用する。
+=end
+
+names = ["SQL", "Perl", "Python", "Ruby"]
+names.each do |name|
+  p name
+end
+
+p "@@@@@@@@@@@@@@@@@@@@@@"
+
+# break
+langs = ["SQL", "Perl", "Python", "Ruby"]
+i_a = 0
+langs.each do |lang|
+  i_a += 1
+  if i_a == 3
+    break
+  end
+  p [i_a, lang]
+end
+
+p "@@@@@@@@@@@@@@@@@@@@@@"
+# next
+i_b = 0
+langs.each do |lang|
+  i_b += 1
+  if i_b == 3
+    next
+  end
+  p [i_b, lang]
+end
