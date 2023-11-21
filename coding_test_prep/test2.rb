247e06eb2aebc1f233b3aef26031546473ad6405
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-21 14:19:13 +0000

    学習更新

--- coding_test_prep/test2.rb
+++ coding_test_prep/test2.rb
@@ -11,3 +11,17 @@ while i <= 10 do
 end
 
 p "1 + 2 + 3 + ... + 10 = #{sum}"
+
+# 問題2
+
+# Ruby
+# # 問題
+# 入力された数値の約数をすべて出力するプログラムを作成してください
+p "整数を入力してください"
+numbers = gets.to_i
+
+for i in 1..numbers
+ if numbers % i == 0
+   p i
+ end
+end
