f9b3dd419b6c7e4f7485058c08e28e78cbfa96bd
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-11-22 01:52:42 +0000

    学習更新

--- times.rb
+++ times.rb
@@ -31,8 +31,57 @@ end
 # Hello ruby
 
 greeting = "Hello ruby"
-count = gets.to_i 
+count = gets.to_i
 
 count.times do
   p greeting
+end
+
+# 下のターミナルウィンドウで、次のプログラムを作って実行してください。
+
+# 1. 標準入力から整数を 1 個受け取る
+# 2. 受け取った整数を表示する
+
+# 3. 受け取った整数回分、以下の処理を繰り返す
+
+# a. さらに標準入力から整数を 1 個受け取る
+# a. その整数が 0 の場合、以下のメッセージを表示する
+# AAAは0
+
+# b. その整数が 0 より大きい場合、以下のメッセージを表示する
+# AAAはプラス
+
+# c. それ以外の場合、以下のメッセージを表示する
+# AAAはマイナス
+
+# * AAA には、その整数を当てはめる
+
+=begin
+ 入力される値
+3
+0
+1
+-1
+
+ 標準入力からの値取得方法はこちらをご確認ください
+
+ 期待する出力値
+3
+0は0
+1はプラス
+-1はマイナス
+=end
+
+count = gets.to_i
+p count
+
+count.times do
+  num = gets.to_i
+  if num == 0
+    puts "#{num}は0"
+  elsif num > 0
+    puts "#{num}はプラス"
+  else
+    puts "#{num}はマイナス"
+  end
 end
