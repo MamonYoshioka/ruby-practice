2cce11b26d9bcaae805163fd23b992753b4efe40
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-12-07 14:00:13 +0000

    algorithm update

--- algo/algo_3.rb
+++ algo/algo_3.rb
@@ -1,14 +1,17 @@
 # 基本交換法
-def bubble(arr)
-  arr.length.times do |i|
-    (arr.length - 1).downto(i + 1) do |j|
-      if arr[j]  < arr[j-1]
-        arr[j], arr[j-1] = arr[j-1], arr[j]
+
+def bubble(array)
+  numbers =  array.length
+  for i in 0 ... numbers
+    for j in 0 ...(numbers - i - 1)
+      if array[j] > array[j + 1]
+        array[j] , array[j + 1] = array[j + 1], array[j]
       end
     end
   end
+  array
 end
 
-arr = [5,9,3,1,2,8,4,7,6]
+array = [5, 9, 3, 1, 2, 8, 4, 7, 6]
 
-p arr
+p bubble(array)
