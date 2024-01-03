e1f0766bbd6f76fe31344b78e42fddee18f1223f
Mamon <yoshiokasnstest@gmail.com>
2024-01-04 00:03:19 +0900

    dir追加

---
+++ ruby_basic/basic.rb
@@ -0,0 +1,29 @@
+# 【真偽値と条件分岐】
+#true
+puts true
+
+#すべての数値
+puts 1
+puts 0
+#puts -1
+
+#すべての文字列
+puts "true"
+puts "false"
+puts ""
+
+# data = find_data 
+# if data
+#     "データがあります。"
+# else
+#     "データがありません。"
+# end
+puts "##########"
+#論理演算子
+t1 = true
+t2 = true
+f1 = false
+
+puts t1 && t2
+puts t1 && f1
+
