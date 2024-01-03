745b3fcc04ad48222ca8de3bdf8a3366fc58a754
Mamon <yoshiokasnstest@gmail.com>
2024-01-04 01:04:15 +0900

    例題練習更新

--- ruby_basic/ruby_check.rb
+++ ruby_basic/ruby_check.rb
@@ -1,2 +1,10 @@
-# check
+#code check
 puts "hello world"
+
+#fizz_buzz check
+
+#3で割り切れる数値を引数に渡すと、"Fizz"と表示される
+#5で割り切れる数値を引数に渡すと、"Buzz"と表示される
+#15で割り切れる数値を引数に渡すと、"FizzBuzz"と表示される
+#それ以外の数値は文字列に変えて返す
+
