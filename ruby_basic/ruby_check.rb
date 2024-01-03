3d1ad1969d20247aaa22870874d03484c32d9277
Mamon <yoshiokasnstest@gmail.com>
2024-01-04 01:50:03 +0900

    例題練習更新2

--- ruby_basic/ruby_check.rb
+++ ruby_basic/ruby_check.rb
@@ -1,5 +1,5 @@
 #code check
-puts "hello world"
+#puts "hello world"
 
 #fizz_buzz check
 
@@ -8,3 +8,23 @@ puts "hello world"
 #15で割り切れる数値を引数に渡すと、"FizzBuzz"と表示される
 #それ以外の数値は文字列に変えて返す
 
+def fizz_buzz(n)
+    if n % 15 == 0
+        "Fizz Buzz"
+    elsif n % 3 == 0
+        "Fizz"
+    elsif n % 5 == 0
+        "Buzz"
+    else
+        n.to_s
+    end
+end
+
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(15)
