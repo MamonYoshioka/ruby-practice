9d380ff9eebeb969620e328121fcc86fb299f8f1
Mamon <yoshiokasnstest@gmail.com>
2024-01-04 00:16:55 +0900

    論理演算子check

--- ruby_basic/basic.rb
+++ ruby_basic/basic.rb
@@ -23,7 +23,19 @@ puts "##########"
 t1 = true
 t2 = true
 f1 = false
-
+puts "###"
+#且つ
 puts t1 && t2
 puts t1 && f1
+
+f2 = false
+puts "###"
+#または
+puts t1 || f1
+puts f1 || f2
+#"且つ"と"または"の組み合わせ
+puts "###"
+puts t1 && t2 || f1 && f2
+puts (t1 && t2) || (f1 && f2)
+puts t1 && (t2 || f1) && f2
 
