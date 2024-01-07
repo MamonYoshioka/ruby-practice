a0f5fc69a2a0cda0efce5b3904cbc1c3fbddaec1
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2024-01-07 17:52:30 +0000

    テスト技法コードサンプル実行

--- ruby_basic/sample_test.rb
+++ ruby_basic/sample_test.rb
@@ -2,6 +2,6 @@ require "minitest/autorun"
 
 class SampleTest < Minitest::Test 
     def test_sample
-        assert_equal "RUBY", "ruby".upcase 
+        assert_equal "RUBY", "ruby".upcase
     end
 end
