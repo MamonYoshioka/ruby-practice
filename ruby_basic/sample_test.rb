1f13e59bb2eac415beb4f6d49a65e564e32e215c
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2024-01-07 18:56:11 +0000

    テスト内容追加

--- ruby_basic/sample_test.rb
+++ ruby_basic/sample_test.rb
@@ -2,6 +2,10 @@ require "minitest/autorun"
 
 class SampleTest < Minitest::Test 
     def test_sample
-        assert_equal "RUBY", "ruby".upcase
+       # assert_equal "RUBY", "ruby".upcase
+    end
+
+    def test_sample
+      assert_equal 15 , 5 * 3
     end
 end
