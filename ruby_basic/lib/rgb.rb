2879b32f9594455cc3e8cb2abf0b941cd17c54e4
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2024-01-16 05:09:49 +0000

    rgbリファクタリング

--- ruby_basic/lib/rgb.rb
+++ ruby_basic/lib/rgb.rb
@@ -4,6 +4,15 @@ def to_hex(r,g,b)
   end
 end
 
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r,g,b].map do |c|
+     c.hex
+  end
+end
+
 # p 0.to_s(16)
 # p 255.to_s(16)
 
@@ -11,3 +20,4 @@ end
 # p "0".rjust(5, "0")
 
 # p 0.to_s(16).rjust(2,"0")
+
--- ruby_basic/test/rgb_test.rb
+++ ruby_basic/test/rgb_test.rb
@@ -7,4 +7,9 @@ class RgbTest < Minitest::Test
     assert_equal "#ffffff", to_hex(255,255,255)
     assert_equal "#043c78", to_hex(4,60,120)
   end
+
+  def test_to_ints
+    assert_equal [0,0,0], to_ints("#000000")
+    assert_equal [255,255,255], to_ints("#ffffff")
+  end
 end
